16b862c8c922c15fb128dc20e131d4b90bc05d7a
Umer <omiomeromi11@gmail.com>
2026-05-27 00:30:33 +0300

    Initial chat list implementation after sucessful trade request acceptance

--- app/(auth)/auth/callback/page.tsx
+++ app/(auth)/auth/callback/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
 
+export default function AuthCallback(){
+  return(
+    <Suspense>
+      <AuthCallbackSearch/>
+    </Suspense>
+  )
+}
 
-export default function AuthCallback() {
+
+export function AuthCallbackSearch() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
